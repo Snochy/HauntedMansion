711cd8cda7d544d1a2b4ad4cf98a2fc7f638c965
Zachary Thorpe <zachauna@gmail.com>
2015-09-27 17:03:47 -0600

    -Lucy Changes

---
+++ Haunted Mansion/Unity Project/Assets/Scripts/GUIs/PlayerTurntable.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerTurntable : MonoBehaviour {
+
+    public Transform target;
+    public float xSpeed = 250.0f;
+    private float x = 0.0f;
+    public float rotationDampening = 3.0f;
+
+	void Update () {
+
+        if (Input.GetMouseButton(0))
+            x += Input.GetAxis("Mouse X") * -xSpeed * 0.02f;
+
+
+        else
+        {
+            float targetRotationAngle = target.eulerAngles.y;
+            float currentRotationAngle = transform.transform.eulerAngles.y;
+            x = Mathf.LerpAngle(currentRotationAngle, targetRotationAngle, rotationDampening * Time.deltaTime);
+        }
+
+        Quaternion rotation = Quaternion.Euler(0, x, 0);
+
+        transform.rotation = rotation;
+	}
+}
